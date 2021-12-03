708dd46b4006923004acb4390f273dfaec92fcbd
youngbamer <0421mk@naver.com>
2021-12-03 22:13:00 +0900

    게시글 작성 기능 구현

--- src/com/sbs/example/board/Article.java
+++ src/com/sbs/example/board/Article.java
@@ -4,4 +4,10 @@ public class Article {
 	public int id;
 	public String title;
 	public String body;
+	
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
 }
--- src/com/sbs/example/board/Main.java
+++ src/com/sbs/example/board/Main.java
@@ -11,13 +11,30 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 
 		List<Article> articles = new ArrayList<>();
+		int lastArticleId = 0;
 
 		while (true) {
 			System.out.printf("명령어) ");
 			String cmd = scanner.nextLine();
 			cmd = cmd.trim();
-			
-			if (cmd.equals("system exit")) {
+
+			if (cmd.equals("article write")) {
+				int id = lastArticleId + 1;
+				String title;
+				String body;
+
+				System.out.println("== 게시글 작성 ==");
+				System.out.printf("제목: ");
+				title = scanner.nextLine();
+				System.out.printf("내용: ");
+				body = scanner.nextLine();
+				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				
+				lastArticleId++;
+
+			} else if (cmd.equals("system exit")) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
 			} else {

6bf9e0366711bde0052c000e94673d4333a33edc
youngbamer <0421mk@naver.com>
2021-12-03 22:51:59 +0900

    JDBC 데이터 삽입 기능 구현

--- src/com/sbs/example/board/test/JDBCTest.java
+++ src/com/sbs/example/board/test/JDBCTest.java
@@ -15,7 +15,7 @@ public class JDBCTest {
 			String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 			// 연결 문자열 생성 => jdbc:mysql://네트워크 주소:포트 번호/연결 DB명?옵션1&옵션2&옵션3...
 			
-			conn = DriverManager.getConnection(url, "sbsst", "sbs123414"); // Mysql JDBC 드라이버 연결 메소드 => (연결 문자열, 데이터베이스 ID, 데이터베이스 PASSWORD)
+			conn = DriverManager.getConnection(url, "root", ""); // Mysql JDBC 드라이버 연결 메소드 => (연결 문자열, 데이터베이스 ID, 데이터베이스 PASSWORD)
 			// Connection 객체를 반환 => 앞으로 conn을 이용해서 쿼리문을 실행할 예정입니다.
 			
 		} catch (ClassNotFoundException e) {

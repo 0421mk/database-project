b8f003e6e12df187b0f82d5af6fc0ec5d1390042
youngbamer <0421mk@naver.com>
2021-12-04 00:16:34 +0900

    게시물 리스팅시 DB에서 내용 조회하는 로직 구현

--- src/com/sbs/example/board/Main.java
+++ src/com/sbs/example/board/Main.java
@@ -3,6 +3,7 @@ package com.sbs.example.board;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -76,6 +77,54 @@ public class Main {
 			} else if (cmd.equals("article list")) {
 
 				System.out.println("== 게시글 목록 ==");
+				
+				Connection conn = null; // DB 접속 객체
+				PreparedStatement pstat = null; // SQL 구문을 실행하는 역할
+				ResultSet rs = null; // ResultSet은 executeQuery 쿼리의 결과값을 저장, next 함수를 통해 데이터 참조 가능
+
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+
+					String sql = "SELECT * FROM article";
+					sql += " ORDER BY id DESC";
+
+					pstat = conn.prepareStatement(sql);
+					rs = pstat.executeQuery(sql);
+
+					while (rs.next()) { // 데이터가 없을때까지 true
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+
+						Article article = new Article(id, regDate, updateDate, title, body);
+						articles.add(article);
+					}
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close(); // 연결 종료
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+					try {
+						if (pstat != null && !pstat.isClosed()) {
+							pstat.close(); // 연결 종료
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 
 				if (articles.size() == 0) {
 					System.out.println("게시글이 존재하지 않습니다.");

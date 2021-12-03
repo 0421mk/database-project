3e1c2ad905a3169bf3816b906b48debef52f9916
youngbamer <0421mk@naver.com>
2021-12-04 02:44:34 +0900

    게시글 상세 보기 기능 구현

--- src/com/sbs/example/board/App.java
+++ src/com/sbs/example/board/App.java
@@ -141,6 +141,39 @@ public class App {
 				System.out.printf("%d / %s\n", article.id, article.title);
 			}
 
+		} else if (cmd.startsWith("article detail")) {
+
+			int id = Integer.parseInt(cmd.split(" ")[2].trim());
+			
+			SecSql sql = new SecSql();
+			
+			sql.append("SELECT COUNT(*)");
+			sql.append("FROM article");
+			sql.append("WHERE id = ?", id);
+			
+			int articlesCount = DBUtil.selectRowIntValue(conn, sql);
+			
+			if(articlesCount == 0) {
+				System.out.printf("%d번 게시글이 존재하지 않습니다.\n", id);
+				return 0;
+			}
+			
+			sql = new SecSql();
+			
+			sql.append("SELECT *");
+			sql.append("FROM article");
+			sql.append("WHERE id = ?", id);
+			
+			Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+			
+			Article article = new Article(articleMap);
+			
+			System.out.printf("번호: %d\n", article.id);
+			System.out.printf("등록날짜: %s\n", article.regDate);
+			System.out.printf("수정날짜: %s\n", article.updateDate);
+			System.out.printf("제목: %s\n", article.title);
+			System.out.printf("내용: %s\n", article.body);
+
 		} else if (cmd.startsWith("article delete")) {
 
 			int id = Integer.parseInt(cmd.split(" ")[2].trim());

739bad961a4da63394d23e8b8c8dcf9114451cfb
youngbamer <0421mk@naver.com>
2021-12-04 14:08:27 +0900

    회원 db.sql 업데이트

--- db.sql
+++ db.sql
@@ -12,3 +12,17 @@ CREATE TABLE article (
 );
 
 DESC article;
+SELECT * FROM article;
+
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(100) NOT NULL,
+    loginPw CHAR(100) NOT NULL,
+    `name` CHAR(100) NOT NULL
+);
+
+DESC article;
+SELECT * FROM `member`;

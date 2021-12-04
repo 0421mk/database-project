ec8d9fa4ddd13f8eab216633b49f282b3fe6ee55
youngbamer <0421mk@naver.com>
2021-12-04 17:14:29 +0900

    게시물 리스팅시 작성자명 나오도록 구현

--- src/com/sbs/example/board/controller/ArticleController.java
+++ src/com/sbs/example/board/controller/ArticleController.java
@@ -122,6 +122,7 @@ public class ArticleController {
 		System.out.printf("번호: %d\n", article.id);
 		System.out.printf("등록날짜: %s\n", article.regDate);
 		System.out.printf("수정날짜: %s\n", article.updateDate);
+		System.out.printf("작성자: %s\n", article.extra_writer);
 		System.out.printf("제목: %s\n", article.title);
 		System.out.printf("내용: %s\n", article.body);
 

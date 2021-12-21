360c9c71f4d9f295fa9ac26d20c5fe0ed4fb987c
youngbamer <0421mk@naver.com>
2021-12-21 17:00:01 +0900

    db.sql 업데이트

--- db.sql
+++ db.sql
@@ -76,3 +76,36 @@ memberId = 1,
 title = '제목5',
 `body` = '내용5',
 hit = 20;
+
+DESC article;
+
+DROP TABLE `like`;
+
+CREATE TABLE `like` (
+    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    articleId INT(10) UNSIGNED NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    likeType TINYINT(1) NOT NULL
+);
+
+DESC `like`;
+SELECT * FROM `like`;
+
+# likeCheck
+SELECT
+CASE WHEN COUNT(*) != 0 THEN likeType ELSE 0
+END AS isRecommend
+FROM `like`
+WHERE articleId = 6 AND memberId = 1;
+
+# getLikeVal
+# 데이터가 없다면 0
+# 데이터가 있다면 해당 데이터 반환
+SELECT COUNT(*)
+FROM `like`
+WHERE articleId = 3 AND likeType = 2;
+
+SELECT * FROM article;
